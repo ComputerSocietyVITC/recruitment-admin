75ef20a0501048828f57eddeeebae5faceb102b9
Aayush Shukla <oreus.aks@gmail.com>
2024-09-07 01:32:48 +0530

    feat: create a custom button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface ButtonProps {
+  text: string;
+  onClick?: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="rounded border border-sky-900 px-4 py-2 mx-2 text-white bg-sky-900 hover:bg-sky-950 transition"
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Button;
